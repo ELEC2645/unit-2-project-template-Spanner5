fb29215fad672ced2da10e50ac027300860d2aa1
Spanner5 <wzxr5943@leeds.ac.uk>
2025-12-07 14:30:49 +0000

    ggg

--- funcs.c
+++ funcs.c
@@ -396,42 +396,17 @@ void menu_item_3(void) {
     - continues till all values are 1.
     */
 
-    // Setting all values in row 1 (the 1st combination of inputs) to zero
-    for (int z = 0; z < Ninput; z++){  
-            array_of_input_combos[0][z] = 0; 
-        }
-
-    // Now, incrementing the values of the inputs for each row, starting from the LSB working backwards:
+    // Now, incrementing the values of the inputs for each row.
     for (int k = 1; k < number_of_input_combinations; k++){ // For each row of inputs
-        for (int l = 0; l < Ninput; l++){ // For each input in a row
-            array_of_input_combos[k][l] = array_of_input_combos[k-1][l]; // Set each row to the previous row, ready for incremening
-        }
-        // Firstly, we need to check the LSB
-        if (array_of_input_combos[k][Ninput-1] == 0){ // if the LSB is zero
-            array_of_input_combos[k][Ninput-1] = 1; // Then increment the LSB - which will not overflow to the next bit
-        }
-        else if (array_of_input_combos[k][Ninput-1] == 1 && Ninput >= 2){ /* if the LSB is one and there are at least 2 inputs 
-            (need to check for this, because if just a buffer has been placed, there will only be 1 input
-            and thus we've already exhausted the combinations) */
 
-            // In this case we'll have set the LSB to zero and increment the next-lowest value bit - and so on...
+        /*
+        k = the VALUE of the input combination,
+        e.g. the 8th input combo of 3 bits is 111 - which is k = 7 and 111 is 7 in binary.
+        So, just subtract the value of each bit from k to get the value of each bit in the value.
+        */
 
-            // So, to check each value in the row and increment accordingly, we need to use a new for loop for this
-
-            for (int l = 2; l < Ninput; l++){ // starting from the next-lowest-bit as a reference point 
-
-
-                // DOESN'T WORK - NEED TO ADJUST --- IT'D JUST SET THE HSB TO 1 EVERY TIME - need to adjust it to do the process once
-                // maybe check if the current bit is one, and that this isn't the HSB, then increment the previous bit.
-                // but the problem is that it keeps incrementing all the bits - just need to do it once per cycle.
-            
-
-                if (array_of_input_combos[k][Ninput-l+1] == 1){ // If the bit lower than the current bit is one:
-                    array_of_input_combos[k][Ninput-l] = 1;     //    then set the current bit to 1...
-                    array_of_input_combos[k][Ninput-l+1] = 0;   //    ...and set the bit lower than the current bit to zero.
-                }
-            }
-        }
+        int combo_value = k;
+        // need to relate each bit to a multiplier based on Ninput - the position ,then subtract from k
         
     }
     
--- testing_random_code.c
+++ testing_random_code.c
@@ -1,6 +1,35 @@
-/*
 
 
+        for (int l = 0; l < Ninput; l++){ // For each input in a row
+            array_of_input_combos[k][l] = array_of_input_combos[k-1][l]; // Set each row to the previous row, ready for incremening
+        }
+        // Firstly, we need to check the LSB
+        if (array_of_input_combos[k][Ninput-1] == 0){ // if the LSB is zero
+            array_of_input_combos[k][Ninput-1] = 1; // Then increment the LSB - which will not overflow to the next bit
+        }
+        else if (array_of_input_combos[k][Ninput-1] == 1 && Ninput >= 2){ /* if the LSB is one and there are at least 2 inputs 
+            (need to check for this, because if just a buffer has been placed, there will only be 1 input
+            and thus we've already exhausted the combinations) */
+
+            // In this case we'll have set the LSB to zero and increment the next-lowest value bit - and so on...
+
+            // So, to check each value in the row and increment accordingly, we need to use a new for loop for this
+
+            for (int l = 2; l < Ninput; l++){ // starting from the next-lowest-bit as a reference point 
+
+
+                // DOESN'T WORK - NEED TO ADJUST --- IT'D JUST SET THE HSB TO 1 EVERY TIME - need to adjust it to do the process once
+                // maybe check if the current bit is one, and that this isn't the HSB, then increment the previous bit.
+                // but the problem is that it keeps incrementing all the bits - just need to do it once per cycle.
+            
+
+                if (array_of_input_combos[k][Ninput-l+1] == 1){ // If the bit lower than the current bit is one:
+                    array_of_input_combos[k][Ninput-l] = 1;     //    then set the current bit to 1...
+                    array_of_input_combos[k][Ninput-l+1] = 0;   //    ...and set the bit lower than the current bit to zero.
+                }
+            }
+            
+        }
 // C Implementation for lineto()
 #include <graphics.h>
 
